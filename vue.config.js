dd5f04a248f5d60bb20fb8c86fb537e2e9be9847
泛舟 <732598668@qq.com>
2021-05-29 01:06:18 +0800

    修改部署应用包时的基本 URL

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // 修改webpack配置
   configureWebpack: {
     resolve: {
       alias: {
@@ -9,6 +10,11 @@ module.exports = {
       }
     }
   },
+  // vue-cli
+  // https://cli.vuejs.org/zh/config/#publicpath
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/admin/'
+    : '/',
   // 输出路径：https://cli.vuejs.org/zh/config/#outputdir
   outputDir: "admin"
 }
