94b65af690e0fa419604f37f62b52c85e16bf0b1
泛舟 <732598668@qq.com>
2021-06-04 21:01:58 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@
   </div>
 </template>
 
+<script>
+export default {
+  created () {
+    window.console.log('虽不能至，心向往之。\r\n\r\n')
+    window.console.log('%cfollow me %c https://github.com/coderyqy', 'color:red', 'color:green')
+  }
+}
+</script>
+
 <style>
 @import './assets/css/base.css';
 @import './assets/css/element.css';
